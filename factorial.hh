a21495f8953942744fbffe11aee0b69613db33fe
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-01-01 17:12:23 +0500

    Define template of immediate factorial function

---
+++ factorial.hh
@@ -0,0 +1,14 @@
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+template<typename TypeN, TypeN N>
+consteval TypeN factorial(){
+  if (0==N || 1==N) {
+    return 1;
+  }
+
+  return N * factorial<TypeN, (N>1)*(N-1)>();
+}
+
+#endif
+
