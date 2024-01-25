fbfa04f227c6a432b63ffaf68e8530b9f8abc618
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-01-26 00:53:35 +0500

    Define class template for solve Clique for size=1

---
+++ clique_1.hh
@@ -0,0 +1,19 @@
+#ifndef CLIQUE_1_HH
+#define CLIQUE_1_HH
+
+template<typename Graph>
+class Clique_1 {
+  private:
+    const Graph graph;
+
+  public:
+    consteval Clique_1(Graph g) : graph{g} {
+    }
+
+    consteval int operator()(void) const {
+      return graph.size();
+    }
+};
+
+
+#endif
