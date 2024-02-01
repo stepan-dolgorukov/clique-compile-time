4e3ac58219065e7464a9b69374ea465943c56996
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-01 22:25:32 +0500

    Define namespace "cct": [c]lique in [c]ompile [t]ime

--- clique.hh
+++ clique.hh
@@ -9,50 +9,53 @@
 #include "clique_4.hh"
 #include "clique_5.hh"
 
-template< std::size_t SizeClique >
-class Clique
+namespace cct
 {
-  private:
-    std::size_t quantity_cliques = 0u;
+  template< std::size_t SizeClique >
+  class Clique
+  {
+    private:
+      std::size_t quantity_cliques = 0u;
 
-  public:
-    template< typename TypeGraph >
-    consteval Clique( TypeGraph graph )
-    {
-      switch ( SizeClique )
+    public:
+      template< typename TypeGraph >
+      consteval Clique( TypeGraph graph )
       {
-        case 1u:
-          quantity_cliques = Clique_1{ graph }();
-          break;
+        switch ( SizeClique )
+        {
+          case 1u:
+            quantity_cliques = Clique_1{ graph }();
+            break;
 
-        case 2u:
-          quantity_cliques = Clique_2{ graph }();
-          break;
+          case 2u:
+            quantity_cliques = Clique_2{ graph }();
+            break;
 
-        case 3u:
-          quantity_cliques = Clique_3{ graph }();
-          break;
+          case 3u:
+            quantity_cliques = Clique_3{ graph }();
+            break;
 
-        case 4u:
-          quantity_cliques = Clique_4{ graph }();
-          break;
+          case 4u:
+            quantity_cliques = Clique_4{ graph }();
+            break;
 
-        case 5u:
-          quantity_cliques = Clique_5{ graph }();
-          break;
+          case 5u:
+            quantity_cliques = Clique_5{ graph }();
+            break;
+        }
       }
-    }
 
-    consteval auto operator()( void ) const
-    {
-      return quantity_cliques;
-    }
-};
+      consteval auto operator()( void ) const
+      {
+        return quantity_cliques;
+      }
+  };
 
-template< std::size_t SizeClique, auto Graph >
-consteval auto clique( void )
-{
-  return Clique< SizeClique >{ Graph }();
+  template< std::size_t SizeClique, auto Graph >
+  consteval auto clique( void )
+  {
+    return Clique< SizeClique >{ Graph }();
+  }
 }
 
 #endif
