a8274fb0b6f17938152ccd3e0d8f0e90de98098b
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-01 21:05:27 +0500

    Insert spaces between brackets and expression inside

--- clique.hh
+++ clique.hh
@@ -9,50 +9,50 @@
 #include "clique_4.hh"
 #include "clique_5.hh"
 
-template<std::size_t SizeClique>
+template< std::size_t SizeClique >
 class Clique
 {
   private:
     std::size_t quantity_cliques = 0u;
 
   public:
-    template<typename TypeGraph>
-    consteval Clique(TypeGraph graph)
+    template< typename TypeGraph >
+    consteval Clique( TypeGraph graph )
     {
-      switch (SizeClique)
+      switch ( SizeClique )
       {
         case 1u:
-          quantity_cliques = Clique_1{graph}();
+          quantity_cliques = Clique_1{ graph }();
           break;
 
         case 2u:
-          quantity_cliques = Clique_2{graph}();
+          quantity_cliques = Clique_2{ graph }();
           break;
 
         case 3u:
-          quantity_cliques = Clique_3{graph}();
+          quantity_cliques = Clique_3{ graph }();
           break;
 
         case 4u:
-          quantity_cliques = Clique_4{graph}();
+          quantity_cliques = Clique_4{ graph }();
           break;
 
         case 5u:
-          quantity_cliques = Clique_5{graph}();
+          quantity_cliques = Clique_5{ graph }();
           break;
       }
     }
 
-    consteval auto operator()(void) const
+    consteval auto operator()( void ) const
     {
       return quantity_cliques;
     }
 };
 
-template<std::size_t SizeClique, auto Graph>
-consteval auto clique(void)
+template< std::size_t SizeClique, auto Graph >
+consteval auto clique( void )
 {
-  return Clique<SizeClique>{Graph}();
+  return Clique< SizeClique >{ Graph }();
 }
 
 #endif
