98e107491bdfef7a85c8af097f32f7aea084c00b
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-03 00:11:11 +0500

    Put "{" on new line

--- clique_2.hh
+++ clique_2.hh
@@ -4,25 +4,30 @@
 namespace cct
 {
   template<typename Graph>
-  class Clique_2 {
+  class Clique_2
+  {
     private:
       const Graph graph;
 
     public:
-      consteval Clique_2(Graph g) : graph{g} {
-      }
+      consteval Clique_2( Graph g ) : graph{ g }
+      {}
 
-      consteval int operator()(void) const {
+      consteval int operator()(void) const
+      {
         int quantity_cliques = 0;
 
-        for (int a=0; a<size(graph); ++a) {
-          for (int b=a+1; b<size(graph); ++b) {
-
-            if (1 == graph[a][b]) {
+        for (int a=0; a<size(graph); ++a)
+        {
+          for (int b=a+1; b<size(graph); ++b)
+          {
+            if (1 == graph[a][b])
+            {
               ++quantity_cliques;
             }
           }
         }
+
         return quantity_cliques;
       }
   };
