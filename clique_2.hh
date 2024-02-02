cd0d7ffe27118a6603cb433268f7508de0ff52be
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-03 00:01:18 +0500

    Put template of class "Clique_2" to namespace "cct"

--- clique_2.hh
+++ clique_2.hh
@@ -1,28 +1,31 @@
 #ifndef CLIQUE_2_HH
 #define CLIQUE_2_HH
 
-template<typename Graph>
-class Clique_2 {
-  private:
-    const Graph graph;
+namespace cct
+{
+  template<typename Graph>
+  class Clique_2 {
+    private:
+      const Graph graph;
 
-  public:
-    consteval Clique_2(Graph g) : graph{g} {
-    }
+    public:
+      consteval Clique_2(Graph g) : graph{g} {
+      }
 
-    consteval int operator()(void) const {
-      int quantity_cliques = 0;
+      consteval int operator()(void) const {
+        int quantity_cliques = 0;
 
-      for (int a=0; a<size(graph); ++a) {
-        for (int b=a+1; b<size(graph); ++b) {
+        for (int a=0; a<size(graph); ++a) {
+          for (int b=a+1; b<size(graph); ++b) {
 
-          if (1 == graph[a][b]) {
-            ++quantity_cliques;
+            if (1 == graph[a][b]) {
+              ++quantity_cliques;
+            }
           }
         }
+        return quantity_cliques;
       }
-      return quantity_cliques;
-    }
-};
+  };
+}
 
 #endif
