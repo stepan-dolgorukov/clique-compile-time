d2b8f10c802503d561b45e6b90fb0d865abcc7ac
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-03 20:50:14 +0500

    Use type "std::size_t" for vertexes

--- clique_2.hh
+++ clique_2.hh
@@ -19,9 +19,9 @@ namespace cct
       {
         std::size_t quantity_cliques{ 0u };
 
-        for (int a = 0; a < size( graph ); ++a)
+        for (std::size_t a{ 0u }; a < size( graph ); ++a)
         {
-          for (int b = a + 1; b < size( graph ); ++b)
+          for (std::size_t b{ 1u + a }; b < size( graph ); ++b)
           {
             if (1 == graph[ a ][ b ])
             {
