86e2be0560395a1db101e73a3574b25f459f3895
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-03 20:41:35 +0500

    Correct the indentation

--- clique_2.hh
+++ clique_2.hh
@@ -3,7 +3,7 @@
 
 namespace cct
 {
-  template<typename Graph>
+  template< typename Graph >
   class Clique_2
   {
     private:
@@ -13,15 +13,15 @@ namespace cct
       consteval Clique_2( Graph g ) : graph{ g }
       {}
 
-      consteval int operator()(void) const
+      consteval int operator()( void ) const
       {
         int quantity_cliques = 0;
 
-        for (int a=0; a<size(graph); ++a)
+        for (int a = 0; a < size( graph ); ++a)
         {
-          for (int b=a+1; b<size(graph); ++b)
+          for (int b = a + 1; b < size( graph ); ++b)
           {
-            if (1 == graph[a][b])
+            if (1 == graph[ a ][ b ])
             {
               ++quantity_cliques;
             }
