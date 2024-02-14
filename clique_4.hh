366f4d988980aa1bd2d408c935e107415f49ce92
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-14 15:03:22 +0500

    Style the source text

--- clique_4.hh
+++ clique_4.hh
@@ -3,47 +3,54 @@
 
 #include "factorial.hh"
 
-template<typename Graph>
-class Clique_4 {
+template< typename Graph >
+class Clique_4
+{
   private:
     const Graph graph;
 
   public:
-    consteval Clique_4(Graph g) : graph{g} {
-    }
+    consteval Clique_4( Graph g ) : graph{ g }
+    {}
 
     auto
-    consteval operator()(void) const {
+    consteval operator()( void ) const
+    {
       int quantity_cliques = 0;
 
-      for (int a=0; a<size(graph); ++a) {
-        for (int b=0; b<size(graph); ++b) {
-
-          if (b == a ||
-              0 == graph[a][b]) {
+      for( int a = 0; a < size( graph ); ++a )
+      {
+        for( int b = 0; b < size( graph ); ++b )
+        {
+          if( b == a ||
+              0 == graph[ a ][ b ] )
+          {
             continue;
           }
 
-          for (int c=0; c<size(graph); ++c) {
-
-            if (c == b ||
+          for( int c = 0; c < size( graph ); ++c )
+          {
+            if( c == b ||
                 c == a ||
-                0 == graph[a][c] ||
-                0 == graph[b][c]) {
+                0 == graph[ a ][ c ] ||
+                0 == graph[ b ][ c ] )
+            {
               continue;
             }
 
-            for (int d=0; d<size(graph); ++d) {
-
-              if (d == a ||
+            for( int d = 0; d < size( graph ); ++d )
+            {
+              if( d == a ||
                   d == b ||
-                  d == c) {
+                  d == c )
+              {
                 continue;
               }
 
-              if (graph[a][d] &&
-                  graph[b][d] &&
-                  graph[c][d]) {
+              if( graph[ a ][ d ] &&
+                   graph[ b ][ d ] &&
+                   graph[ c ][ d ] )
+              {
                 ++quantity_cliques;
               }
             }
@@ -51,7 +58,7 @@ class Clique_4 {
         }
       }
 
-      return quantity_cliques / factorial<int, 4>();
+      return quantity_cliques / factorial< int, 4 >();
     }
 };
 
