5bc850fc5c4520db424286debb2663fc71d330e0
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-16 09:57:26 +0500

    Put template of class "Clique_3" to namespace "cct"

--- clique_3.hh
+++ clique_3.hh
@@ -5,50 +5,53 @@
 
 #include "factorial.hh"
 
-template< typename Graph >
-class Clique_3
+namespace cct
 {
-  private:
-    const Graph graph;
+  template< typename Graph >
+  class Clique_3
+  {
+    private:
+      const Graph graph;
 
-  public:
-    consteval Clique_3( Graph g ) : graph{ g }
-    {}
+    public:
+      consteval Clique_3( Graph g ) : graph{ g }
+      {}
 
-    auto
-    consteval operator()( void ) const
-    {
-      std::size_t quantity_cliques{ 0u };
-
-      for( std::size_t a{ 0u }; a < size( graph ); ++a )
+      auto
+      consteval operator()( void ) const
       {
-        for( std::size_t b{ 0u }; b < size( graph ); ++b )
-        {
-          if( b == a ||
-              0 == graph[ a ][ b ] )
-          {
-            continue;
-          }
+        std::size_t quantity_cliques{ 0u };
 
-          for( std::size_t c{ 0u }; c < size( graph ); ++c )
+        for( std::size_t a{ 0u }; a < size( graph ); ++a )
+        {
+          for( std::size_t b{ 0u }; b < size( graph ); ++b )
           {
-            if( c == a ||
-                c == b )
+            if( b == a ||
+                0 == graph[ a ][ b ] )
             {
               continue;
             }
 
-            if( 1 == graph[ b ][ c ] &&
-                1 == graph[ c ][ a ] )
+            for( std::size_t c{ 0u }; c < size( graph ); ++c )
             {
-              ++quantity_cliques;
+              if( c == a ||
+                  c == b )
+              {
+                continue;
+              }
+
+              if( 1 == graph[ b ][ c ] &&
+                  1 == graph[ c ][ a ] )
+              {
+                ++quantity_cliques;
+              }
             }
           }
         }
-      }
 
-      return quantity_cliques / factorial< int, 3 >();
-    }
-};
+        return quantity_cliques / factorial< int, 3 >();
+      }
+  };
+}
 
 #endif
