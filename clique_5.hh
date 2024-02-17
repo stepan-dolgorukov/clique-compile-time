7d60cd96d4197835c20fc4b3a508e3df80490255
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-02-17 11:02:26 +0500

    Put template of class "Clique_5" to namespace "cct"

--- clique_5.hh
+++ clique_5.hh
@@ -1,79 +1,82 @@
 #ifndef CLIQUE_5_HH
 #define CLIQUE_5_HH
 
-template< typename Graph >
-class Clique_5
+namespace cct
 {
-  private:
-    const Graph graph;
+  template< typename Graph >
+  class Clique_5
+  {
+    private:
+      const Graph graph;
 
-  public:
-    consteval Clique_5( Graph g ) : graph{ g }
-    {}
+    public:
+      consteval Clique_5( Graph g ) : graph{ g }
+      {}
 
-    int
-    consteval operator()( void ) const
-    {
-      int quantity_cliques = 0;
-
-      for( int a = 0; a < size( graph ); ++a )
+      int
+      consteval operator()( void ) const
       {
-        for( int b = 0; b < size( graph ); ++b )
-        {
-          if( b == a ||
-              0 == graph[ a ][ b ] )
-          {
-            continue;
-          }
+        int quantity_cliques = 0;
 
-          for( int c = 0; c < size( graph ); ++c )
+        for( int a = 0; a < size( graph ); ++a )
+        {
+          for( int b = 0; b < size( graph ); ++b )
           {
-            if( c == b ||
-                c == a ||
-                0 == graph[ a ][ c ] ||
-                0 == graph[ b ][ c ] )
+            if( b == a ||
+                0 == graph[ a ][ b ] )
             {
               continue;
             }
 
-            for( int d = 0; d < size( graph ); ++d )
+            for( int c = 0; c < size( graph ); ++c )
             {
-              if( d == a ||
-                  d == b ||
-                  d == c ||
-                  0 == graph[ a ][ d ] ||
-                  0 == graph[ b ][ d ] ||
-                  0 == graph[ c ][ d ] )
+              if( c == b ||
+                  c == a ||
+                  0 == graph[ a ][ c ] ||
+                  0 == graph[ b ][ c ] )
               {
                 continue;
               }
 
-              for( int e = 0; e < size( graph ); ++e )
+              for( int d = 0; d < size( graph ); ++d )
               {
-                if( e == a ||
-                    e == b ||
-                    e == c ||
-                    e == d )
+                if( d == a ||
+                    d == b ||
+                    d == c ||
+                    0 == graph[ a ][ d ] ||
+                    0 == graph[ b ][ d ] ||
+                    0 == graph[ c ][ d ] )
                 {
                   continue;
                 }
 
-                if( 1 == graph[ a ][ e ] &&
-                    1 == graph[ b ][ e ] &&
-                    1 == graph[ c ][ e ] &&
-                    1 == graph[ d ][ e ] )
+                for( int e = 0; e < size( graph ); ++e )
                 {
-                    ++quantity_cliques;
-                }
-              }
+                  if( e == a ||
+                      e == b ||
+                      e == c ||
+                      e == d )
+                  {
+                    continue;
+                  }
 
+                  if( 1 == graph[ a ][ e ] &&
+                      1 == graph[ b ][ e ] &&
+                      1 == graph[ c ][ e ] &&
+                      1 == graph[ d ][ e ] )
+                  {
+                      ++quantity_cliques;
+                  }
+                }
+
+              }
             }
           }
         }
-      }
 
-      return quantity_cliques;
-    }
-};
+        return quantity_cliques;
+      }
+  };
+}
 
 #endif
